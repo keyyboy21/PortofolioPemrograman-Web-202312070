c5759e377546643f27f9866237e97cba9423665c
muhamad rizky <m.risky14051999@gmail.com>
2025-07-14 14:31:02 +0800

    Initial commit: Menambahkan proyek Modul 5 Dasar-dasar PHP

---
+++ modul_5/constant_latihan.php
@@ -0,0 +1,4 @@
+<?php
+define("NAMA_MATA_KULIAH", "PEMROGRAMAN WEB");
+echo "Mata kuliah saya " . NAMA_MATA_KULIAH;
+?>
---
+++ modul_5/syntax.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Halaman php pertamaku</h1>
+        <?php
+        echo "Halo,Dunia!"
+        ?>
+    </body>
+</html>
